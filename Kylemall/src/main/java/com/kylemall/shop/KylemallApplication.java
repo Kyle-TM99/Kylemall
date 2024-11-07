d75838bc7badbf62dc6573c0620796b1884b854a
kyle99 <rlaxoals9977@gmail.com>
2024-11-07 18:33:38 +0900

    프로젝트 초기 설정 (build.gradle, application.properties)

---
+++ Kylemall/src/main/java/com/kylemall/shop/KylemallApplication.java
@@ -0,0 +1,13 @@
+package com.kylemall.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KylemallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KylemallApplication.class, args);
+	}
+
+}
---
+++ Kylemall/src/test/java/com/kylemall/shop/KylemallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kylemall.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KylemallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
